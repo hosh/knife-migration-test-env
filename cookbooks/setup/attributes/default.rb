b9031dd9829f27626459ef0b84a459f636f5f94e
Ho-Sheng Hsiao <hosh@opscode.com>
2013-01-29 16:37:02 -0800

    [VM] Copy chef-11 knife keys to shared folder

--- cookbooks/setup/attributes/default.rb
+++ cookbooks/setup/attributes/default.rb
@@ -8,6 +8,7 @@ case node[:setup][:chef_server_version]
 when 'chef-10'
   default[:setup][:admin_keys] = '/etc/chef/validation.pem /etc/chef/webui.pem'
 when 'chef-11'
+  default[:setup][:admin_keys] = '/etc/chef-server/admin.pem'
 else
   fail "Define setup/chef_server_version"
 end
